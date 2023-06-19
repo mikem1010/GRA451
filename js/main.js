ae3e7c378d7b1f8a5d0e9fbd8be86ec7c3b40047
mikem1010 <135393585+mikem1010@users.noreply.github.com>
2023-06-19 16:59:22 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,11 @@
-/* Add this CSS to your main.css file or a separate CSS file */
-#popup-container {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: none;
-  justify-content: center;
-  align-items: center;
-}
+// Add this jQuery code to your main.js file or a separate JavaScript file
+$(document).ready(function() {
+  // Open the pop-up when the page loads
+  $("#popup-container").fadeIn();
 
-#popup-content {
-  background-color: #fff;
-  padding: 20px;
-  text-align: center;
-}
-
-#close-button {
-  margin-top: 10px;
-}
+  // Close the pop-up when the "Close" button is clicked
+  $("#close-button").click(function() {
+    $("#popup-container").fadeOut();
+  });
+});
 
