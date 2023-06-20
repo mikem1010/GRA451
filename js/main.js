9e93dd06216c346a15ecabdb888ad8dcd0568124
mikem1010 <135393585+mikem1010@users.noreply.github.com>
2023-06-19 19:38:17 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,4 +9,25 @@ $(document).ready(function() {
   });
 });
 
+// Add this JavaScript code to your main.js file or a separate JavaScript file
+$(document).ready(function() {
+  // Handle form submission
+  $("#contact-form").submit(function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get the form data
+    var firstName = $("#first-name").val();
+    var lastName = $("#last-name").val();
+    var email = $("#email").val();
+
+    // Perform validation if needed
+    // ...
+
+    // Perform form processing or send data to the server
+    // ...
+
+    // Reset the form fields after submission
+    $("#contact-form")[0].reset();
+  });
+});
 
