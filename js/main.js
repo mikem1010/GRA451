7e291db020c1a295188c0ebc0251d9d863771655
mikem1010 <135393585+mikem1010@users.noreply.github.com>
2023-06-19 17:08:32 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
 // Update the jQuery code in your main.js file or a separate JavaScript file
 $(document).ready(function() {
   // Open the pop-up when the page loads
-  $("#popup-container").fadeIn();
-  $("#popup-content").delay(500).fadeIn();
+  $("#popup-container").addClass("open");
 
   // Close the pop-up when the "Close" button is clicked
   $("#close-button").click(function() {
-    $("#popup-content").fadeOut(function() {
-      $("#popup-container").fadeOut();
-    });
+    $("#popup-container").removeClass("open");
   });
 });
-
-
